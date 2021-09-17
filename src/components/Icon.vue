a8d3a3703a87f929ac470823e0cde6d880ae2560
Jacky <bestfish16@gmail.com>
2021-09-17 15:48:16 +0800

    feat: import svg 套件
    why:
    icon需使用svg格式
    
    how:
    引入svg-sprite-loader套件及撰寫 vue.config 設定

---
+++ src/components/Icon.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+svg.icon
+  use(:xlink:href="`#${name}`")
+</template>
+
+<script>
+export default {
+  name: "Icon",
+  props: {
+    name: String
+  },
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="sass">
+.icon
+    color: white
+svg
+    fill: white
+    width: 100%
+    height: 100%
+</style>
