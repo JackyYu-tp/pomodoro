a6cc0587ebd90a43c5eaa565a98bb1347e95a6d3
Jacky <bestfish16@gmail.com>
2021-11-03 00:30:25 +0800

    feat: modal製作
    modal 元件製作

---
+++ src/components/Modal.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+.modal
+  slot
+</template>
+
+<script>
+export default {
+  name: "Modal",
+  props: {},
+  data() {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style lang="sass" scoped>
+.modal
+  position: absolute
+  top: 0
+  left: 0
+  width: 100vw
+  height: 100vh
+  background-color: rgba(0, 0, 0, .4)
+  display: flex
+  align-items: center
+  justify-content: center
+</style>
